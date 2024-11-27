4b6325641dd5ec1eef03b652a314d328d852679a
Spencer <snystrom@users.noreply.github.com>
2024-11-27 14:58:51 -0500

    Add helpers to create and check options.
    Options can now be created from values based on user-defined predicates.

--- R/option-rs.R
+++ R/option-rs.R
@@ -1,11 +1,63 @@
-option <- function(x) {
-  if (missing(x))
-    return(none)
+#' Default predicates defining None values.
+#'
+#' Values that are missing, `NULL`, or `NA` will be considered `None`.
+#'
+#' @return list(missing, is.null, is.na)
+#' @seealso missing is.null is.na
+#' @export
+default_none_predicates <- function() {
+  list(
+    missing,
+    is.null,
+    is.na
+  )
+}
 
-  if (is.null(x))
-    return(none)
+#' @param x a value to convert to `some()` or `none`.
+#' @param .none_predicates a list of predicate functions that evaluate to
+#'   TRUE/FALSE. If a predicate evalutes to `TRUE`, we return `none`, else we
+#'   return `some(x)`. Default: `default_none_predicates()`
+#'
+#' @return an option
+#' @seealso default_none_predicates
+option <- function(x, .none_predicates = default_none_predicates()) {
+  stopifnot(".none_predicates must be a list" = is.list(.none_predicates))
+
+  if (is.null(.none_predicates)) {
+    stop(".none_predicates cannot be NULL. Use `list(is.null)` instead.", call. = FALSE)
+  }
+
+  # TODO: this doesn't work cause of lazy eval on .none_predicates...  It's
+  # problematic to not check at this level because otherwise if we check in the
+  # loop, a .none_predicate may be invalid and we would never catch it if we
+  # never evaluate it before returning...
+  #is_fun <- vapply(eval(.none_predicates), is.function, logical(length(.none_predicates)))
+  #stopifnot("all none_predicates must be functions" = all(is_fun))
+
+  for (.p in .none_predicates) {
+    result <- .p(x)
+    if (!is.logical(result) || length(result) > 1) {
+      stop("Predicate functions must return TRUE/FALSE.", call. = FALSE)
+    }
+    if (result) {
+      return(none)
+    }
+  }
 
   some(x)
+}
+
+#' Fetch the option_t of the Some variant of an Option
+#' (means you can check type with out unwrapping)
+#' @noRd
+option_t <- function(x) {
+  UseMethod("option_t")
+}
+
+#' @export
+option_t.option <- function(x) {
+  stopifnot("Cannot check Option<T> of None" = !is_none(x))
+  attr(x, "option_t", exact = TRUE)
 }
 
 #' @export
@@ -28,8 +80,8 @@ none <- structure(list(),
 #' @export
 print.option <- function(x, ...) {
   if (is_some(x)) {
-    # TODO: this is not great for big stuff...
-    cat(paste0("some(", class(unwrap(x)), ")\n"))
+    cat(paste0("some(", option_t(x), ")\n"))
+    print(unwrap(x))
   }
 
   if (is_none(x)) {
@@ -39,12 +91,14 @@ print.option <- function(x, ...) {
   return(invisible(x))
 }
 
+#' @export
 is_option <- function(x) {
   inherits(x, "option")
 }
 
 option_enum <- function(x, is = c("some", "none")) {
-  # we're not match.arg'ing for speed
+  # we're not match.arg'ing for speed since this is internal-only,
+  # default `is` arg is just as a reminder
   attr(x, "enum") == is
 }
 
